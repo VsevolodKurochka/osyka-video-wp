bdadec8ae5f86e0d0f4e5b055434593187eaef46
Vsevolod Kurochka <dramdoobas1@gmail.com>
2018-02-09 13:10:21 +0200

    Update Front Page

--- functions.php
+++ functions.php
@@ -119,13 +119,15 @@ add_action( 'widgets_init', 'videooperator_widgets_init' );
 function videooperator_scripts() {
 	wp_enqueue_style( 'videooperator-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'videooperator-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'videooperator-libs', get_template_directory_uri() . '/js/libs.min.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'videooperator-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'videooperator-jquery-main', get_template_directory_uri() . '/js/jquery.main.js', array(), '20151215', true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+	wp_enqueue_script( 'videooperator-jquery-vanilla', get_template_directory_uri() . '/js/vanilla.main.js', array(), '20151215', true );
+
+	// if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+	// 	wp_enqueue_script( 'comment-reply' );
+	// }
 }
 add_action( 'wp_enqueue_scripts', 'videooperator_scripts' );
 
--- header.php
+++ header.php
@@ -14,43 +14,63 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="HandheldFriendly" content="true">
+	<meta name="MobileOptimized" content="width">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="#000000">
+	<meta name="msapplication-TileColor" content="#000000">
+	<meta name="format-detection" content="telephone=no">
+	<meta name="format-detection" content="address=no">
 
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'videooperator' ); ?></a>
+	<div class="fixed fixed_md-top">
+		<div class="nav nav_style" id="js-navigation">
+			<div class="nav__container container">
+				<div class="nav__row row">
+					<div class="nav__logo col-8 col-md-3">
+						<button class="hamburger hamburger_effect-2" type="button" id="js-nav-hamburger"><span class="hamburger__lines"><span></span><span></span><span></span><span></span></span>
+						</button>
+						<div class="nav__logo-content">
+							<p class="nav__logo-content-text"><span>Oleksii</span> Osyka</p>
+						</div>
+					</div>
+					<nav class="nav__menu col-12 col-md-10" id="js-navigation-menu">
+						<?php
+							wp_nav_menu( array(
+								'theme_location' => 'menu-1',
+								'menu_id'        => 'primary-menu',
+							) );
+						?>
+					</nav>
+					<div class="nav__addition col-4 col-md-2" id="js-nav-addition">
+						<ul class="social social_white">
+							<li class="social__item"><a class="social__item-link" href="#"><i class="fab fa-facebook-f"></i></a>
+							</li>
+							<li class="social__item"><a class="social__item-link" href="#"><i class="fab fa-instagram"></i></a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="page" class="site">
+		<header class="header header_full header_background">
+			<div class="header__video">
+				<iframe class="header__video-iframe" src="https://www.youtube.com/embed/W0LHTWG-UmQ?controls=0&amp;showinfo=0&amp;rel=0&amp;autoplay=1&amp;loop=1&amp;playlist=W0LHTWG-UmQ" frameborder="0" allowfullscreen="allowfullscreen">
+				</iframe>
+			</div>
+			<div class="header__container container">
+				<h1 class="header__title header__title_big"><strong>Oleksii</strong> Osyka</h1>
+				<h2 class="header__subtitle">DP, Cinematographer, Director, Cameraman</h2>
+				<button class="btn btn_border-white effect effect_bounce-top">Contact Me</button>
+			</div>
+		</header>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'videooperator' ); ?></button>
-			<?php
-				wp_nav_menu( array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				) );
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+		<div id="content" class="site-content">
